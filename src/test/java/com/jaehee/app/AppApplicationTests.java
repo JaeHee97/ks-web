926a0fb992e46d468857bfe3b1eccf7b5e0fc4cd
Jaehee Jang <wogml6026@naver.com>
2020-04-10 22:44:21 +0900

    init

---
+++ src/test/java/com/jaehee/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaehee.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
